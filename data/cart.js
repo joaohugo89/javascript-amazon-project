2dd0c6e64c8d6cc3d1fff63371ba23907ecb719c
Joao Hugo <joaohugosoares89@outlook.com>
2024-10-16 16:04:28 -0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,18 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-}, {
-    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity: 4,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    }, {
+        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+        quantity: 4,
+    }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId){
     let matchingItem;
@@ -19,6 +27,8 @@ export function addToCart(productId){
     } else {
         cart.push({productId:productId, quantity:1});    
     }
+
+    saveToStorage();
 };
 
 export function removeFromCart(productId){
@@ -31,4 +41,5 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 }
