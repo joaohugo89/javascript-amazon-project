524a8fcbb4f2ff61783b1dcbd4e96400f96f74a4
Joao Hugo <joaohugosoares89@outlook.com>
2024-10-17 14:25:46 -0300

    Save data and generate HTMl for delivery options

--- data/cart.js
+++ data/cart.js
@@ -4,9 +4,11 @@ if (!cart) {
     cart = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 2,
+        deliveryOptionId: '1'
     }, {
         productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
         quantity: 4,
+        deliveryOptionId: '2'
     }];
 }
 
@@ -25,7 +27,7 @@ export function addToCart(productId){
     if (matchingItem){
         matchingItem.quantity += 1;
     } else {
-        cart.push({productId:productId, quantity:1});    
+        cart.push({productId:productId, quantity:1, deliveryOptionId:'1'});    
     }
 
     saveToStorage();
