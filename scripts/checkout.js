ea4111bffd427bfd9e46b32b212114ba8afa12cc
Joao Hugo <joaohugosoares89@outlook.com>
2024-10-17 21:55:46 -0300

    Save data and generate HTML for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -24,10 +24,11 @@ cart.forEach((cartItem) => {
             deliveryOption = option;
         }
     });
-
+    
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
+    
 
     cartSummaryHTML += `
         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -82,7 +83,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId
         
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option"
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio" 
                     ${isChecked ? 'checked': ''}
                     class="delivery-option-input"
@@ -112,5 +115,12 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
             `.js-cart-item-container-${productId}`
         );
         container.remove();
+    });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
     });
 });
