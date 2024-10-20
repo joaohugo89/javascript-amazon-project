f671bce03f8bf0f6d3e3516ca767887ef3bac9b9
Joao Hugo <joaohugosoares89@outlook.com>
2024-10-20 20:54:26 -0300

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity: 4,
-        deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+            quantity: 4,
+            deliveryOptionId: '2'
+        }];
+    }
 }
 
 function saveToStorage(){
